7bfcf7c84f3b5a589b4b43656526631ab229e464
aouf <38589565+Aouf-AlDabal@users.noreply.github.com>
2020-05-08 21:51:22 +0300

    Add files

---
+++ healthy/articles.php
@@ -0,0 +1,222 @@
+
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+   
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<link href="https://gitcdn.github.io/bootstrap-toggle/2.2.2/css/bootstrap-toggle.min.css" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	
+    <title>Articles</title>
+  </head>
+  <body>
+    <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+      <div class="container">
+        <a href="index.html" class="navbar-brand">Healthy life </a>
+        <button class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item ">
+              <a href="index.html" class="nav-link">Home</a>
+            </li>
+            <li class="nav-item active">
+              <a href="articles.php" class="nav-link"> Articles </a>
+            </li>
+            <li class="nav-item ">
+              <a href="signin.php" class="nav-link">live chat </a>
+            </li>
+            <li class="nav-item">
+              <a href="about.html" class="nav-link">About Us</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <section id="blog" class="py-3  ">
+    <header>
+      <div class="container  mx-auto">
+ 
+        <div class="row">
+          <div class="col-md-6 m-auto text-center">
+            <div class="shadow p-1 mb-2 bg-white rounded text-dark "><h1>Articles</h1></div>
+          </div>
+        </div>
+      </div>
+    </header>
+    <div class="container  mx-auto">
+     
+     <div class="row">
+       <div class="col-md-4 mb-4"> 
+        
+        <div class="card shadow h-100" style="width: 20rem;">
+          <div class="inner">
+            <img src="images/food group.jpg" class="card-img-top" alt="...">
+          </div>
+         
+          <div class="card-body ">
+            <h5 class="card-title font-weight-bold">The five main food groups</h5>
+            <p class="card-text  ">There are five main food groups that we need to eat food from every day to stay healthy.
+               Food is the fuel for our body and we need the fuel for energy, 
+               to help our body grow and repair itself, and to keep warm.
+            </p>
+            
+          </div>
+          <a href="articles1.html" class="btn  btn-success btn-lg btn-lg " role="button" aria-pressed="true">read more...</a>
+        </div>
+      </div>
+      <div class="col mb-4"> 
+        
+        <div class="card shadow h-100" style="width: 20rem;">
+          <div class="inner">
+            <img   src="images/time to eat.jpg" class="card-img-top" alt="...">
+          </div>
+         
+          <div class="card-body ">
+            <h5 class="card-title font-weight-bold"> Six Popular Ways for Intermittent Fasting</h5>
+            <p class="card-text  ">Intermittent fasting has recently become a health trend. It’s claimed to cause weight loss, improve metabolic health, and perhaps even extend lifespan.
+
+                 Several methods of this eating pattern exist.
+
+                 Every method can be effective, but figuring out which one works best depends on the individual.
+            </p>
+            
+          </div>
+          <a href="articles2.html" class="btn  btn-success btn-lg btn-lg " role="button" aria-pressed="true">read more...</a>
+        </div>
+      </div>
+      <div class="col mb-4"> 
+        
+        <div class="card shadow h-100" style="width: 20rem;">
+          <div class="inner">
+            <img  class="h-65" src="images/eat.jpg" class="card-img-top" alt="...">
+          </div>
+         
+          <div class="card-body ">
+            <h5 class="card-title font-weight-bold">why obesity happens?</h5>
+            <p class="card-text"> 
+            Obesity is generally caused by eating too much and moving too little.
+   If you consume high amounts of energy, particularly fat and sugars, but do not burn off the energy through exercise and physical activity, much of the surplus energy will be stored by the body as fat.
+
+            </p>
+            
+          </div>
+          <a href="articles3.html" class="btn  btn-success btn-lg btn-lg " role="button" aria-pressed="true">read more...</a>
+        </div>
+      </div>
+      <div class="col mb-4"> 
+        
+        <div class="card shadow h-100" style="width: 20rem;">
+          <div class="inner">
+            <img  src="images/obesity2.jpg" class="card-img-top" alt="...">
+          </div>
+         
+          <div class="card-body ">
+            <h5 class="card-title font-weight-bold">The Impact of Obesity</h5>
+            <p class="card-text  "> Obesity can cause a lot of damage to your body.
+               People with severe obesity are more likely to have other diseases. 
+               These include type 2 diabetes, high blood pressure, sleep apnea, and many more. 
+               Combined with obesity, these diseases may lead people to have a lower quality of health.
+               In some cases, these can lead to disability or early death.
+            </p>
+            
+          </div>
+          <a href="articles4.html" class="btn  btn-success btn-lg btn-lg " role="button" aria-pressed="true">read more...</a>
+        </div>
+      </div>
+       <div class="col mb-4">
+        <div class="card shadow h-100" style="width: 20rem;">
+          <div class="inner">
+            <img src="images/obesity3.jpg" class="card-img-top" alt="...">
+          </div>
+          
+         
+          <div class="card-body ">
+            <h5 class="card-title font-weight-bold">What is obesity</h5>
+            <p class="card-text  ">Obesity is a medical condition that occurs when a person carries excess weight or body fat that might affect their health. 
+              A doctor will usually suggest that a person has obesity if they have a high body mass index.
+            </p>
+           
+          </div>
+          <a href="articles5.html" class="btn  btn-success btn-lg btn-lg " role="button" aria-pressed="true">read more...</a>
+        </div>
+      </div>
+       <div class="col mb-4">
+        <div class="card shadow h-100" style="width: 20rem;">
+          <div class="inner">
+            <img  src="images/vegan1.jpeg" class="card-img-top" alt="...">
+          </div>
+          <div class="card-body">
+            <h5 class="card-title font-weight-bold">The Vegan Diet</h5>
+            <p class="card-text  ">The vegan diet has become very popular.
+
+Increasingly more people have decided to go vegan for ethical, environmental or health reasons.
+
+When done right, such a diet may result in various health benefits, including a trimmer waistline and improved blood sugar control.
+
+Nevertheless, a diet based exclusively on plant foods may, in some cases, increase the risk of nutrient deficiencies.
+
+This article is a detailed beginner's guide to the vegan diet. It aims to cover everything you need to know, so you can follow a vegan diet the right way.
+            </p>
+           
+          </div>
+          <a href="articles6.html" class="btn  btn-success btn-lg btn-lg " role="button" aria-pressed="true">read more...</a>
+      
+      </div>
+     </div>
+   
+   
+      </div>
+    </div>
+
+  </div>
+  </section>
+    
+
+
+
+			<!-- FOOTER -->
+  <footer id="main-footer" class="text-center p-4">
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <p>Copyright &copy;
+            <span id="year"></span> Healthy life</p>
+        </div>
+      </div>
+    </div>
+  </footer>	
+	  
+			 
+		
+	  <!-- /.container -->
+	
+	 <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+   <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+	<script src="https://gitcdn.github.io/bootstrap-toggle/2.2.2/js/bootstrap-toggle.min.js"></script>
+
+	<script>
+	
+
+  $('#year').text(new Date().getFullYear());
+
+	
+	</script>
+
+   
+  </body>
+</html>
---
+++ healthy/include/headersingin.php
@@ -0,0 +1,31 @@
+
+
+
+
+<link rel="stylesheet" type="text/css" href="css/style.css">
+
+  <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+      <div class="container">
+        <a href="index.html" class="navbar-brand">Healthy life </a>
+        <button class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item   ">
+              <a href="index.html" class="nav-link">Home</a>
+            </li>
+            <li class="nav-item ">
+              <a href="articles.php" class="nav-link"> Articles </a>
+            </li>
+            <li class="nav-item active">
+              <a href="signin.php" class="nav-link">live chat </a>
+            </li>
+            <li class="nav-item">
+              <a href="about.html" class="nav-link">About Us</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  
